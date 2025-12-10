c73131d5bfb38988333929349c91709b772e7654
y-hirakaw <hiraymail@gmail.com>
2025-12-10 12:24:38 +0900

    test: Add test file for detailed metrics validation

---
+++ test_detailed_metrics.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// TestFunction demonstrates detailed metrics tracking
+// Updated by human developer
+func TestFunction() {
+	fmt.Println("This is a test for detailed metrics")
+	fmt.Println("Line 2 - modified by human")
+	fmt.Println("Line 3 - modified by human")
+	// Line 4 and 5 removed by human
+	fmt.Println("New line 6 added by human")
+	fmt.Println("New line 7 added by human")
+}
