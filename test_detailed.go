d0a56234bb3ff584edd8077e362c7e230f8daf10
y-hirakaw <hiraymail@gmail.com>
2025-12-10 12:31:33 +0900

    test: Add initial test file for detailed metrics

---
+++ test_detailed.go
@@ -0,0 +1,11 @@
+package main
+
+// Human modified version
+func TestDetailedMetrics() {
+	println("Modified by human line 1")
+	println("Modified by human line 2")
+	println("Modified by human line 3")
+	println("Human added new line 1")
+	println("Human added new line 2")
+	println("Human added new line 3")
+}
